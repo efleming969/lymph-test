cae5ef1ac09b3f579375d3f9a37029bc5b1bce70
Erick Fleming <efleming969@gmail.com>
2016-05-29 16:58:32 -0500

    initial take on exceptionless testing

--- index.js
+++ index.js
@@ -1,53 +1,124 @@
-exports.SuiteRunner = {
-  run: function( suites, done ) {
-    Object.keys( suites ).forEach( function( suiteName ) {
-      exports.TestRunner.run( suites[ suiteName ], done )
-    } )
+var R = require( "ramda" )
+var JSDiff = require( "diff")
+var equals = require( "equals" )
+
+var diffForConsole = function( msg, a, b )
+{
+  var getPartColor = function( part )
+  {
+    if( part.added )
+      return "green"
+    else if( part.removed )
+      return "red"
+    else
+      return "grey"
   }
+
+  var getColorStyle = function( color )
+  {
+    return "color:" + color
+  }
+
+  var getPartValue = function( part )
+  {
+    return "%c" + part.value
+  }
+
+  var getDiffs = function( a, b )
+  {
+    if ( typeof a === "string" )
+      return JSDiff.diffChars( a, b )
+    else
+      return JSDiff.diffJson( a, b )
+  }
+
+  var diffs = getDiffs( a, b )
+
+  var values = R.map( getPartValue, diffs )
+  var styles = R.map( R.pipe( getPartColor, getColorStyle ), diffs )
+
+  return R.prepend(
+    "%c" + msg + values.join( "|" ),
+    R.prepend( "color:black", styles )
+  )
 }
 
-exports.TestRunner = {
-  run: function( suiteFn, suiteDone ) {
-    var contexts = []
-    suiteFn( {
-      context: function( contextName, contextFn ) {
-        var context = { name: contextName, tests: [] }
-        contextFn( {
-          test: function( testName, testFn ) {
-            var test = { name: testName, asserts: [], logItems: [], error: "" }
-            try {
-              testFn( {
-                assert: function ( value ) { test.asserts.push( value ) },
-                log: function( value ) { test.logItems.push( value ) },
-                done: function() {
-                  context.tests.push( test )
+exports.run = function( name, givens )
+{
+  Object.keys( givens ).forEach(
+    function( given )
+    {
+      givens[ given ](
+        function( whens )
+        {
+          Object.keys( whens ).forEach(
+            function( when )
+            {
+              var thenWasCalled = false
+
+              whens[ when ](
+                function( thens )
+                {
+                  thenWasCalled = true
+
+                  Object.keys( thens ).forEach(
+                    function( then )
+                    {
+                      if( thens[ then ].length === 2 )
+                      {
+                        if( equals( thens[ then ][ 0 ], thens[ then ][ 1 ] ) )
+                        {
+                          console.log( name + ": " + given + " ! " + when + " > " + then )
+                        }
+                        else
+                        {
+                          console.warn.apply(
+                            console,
+                            diffForConsole(
+                              name + ": " + given + " ! " + when + " > " + then + ": ",
+                              thens[ then ][ 0 ], thens[ then ][ 1 ]
+                            )
+                          )
+                        }
+                      }
+                      else if ( thens[ then ].length === 1 )
+                      {
+                        if ( Boolean( thens[ then ][ 0 ] ) )
+                        {
+                          console.log( name + ": " + given + " ! " + when + " > " + then )
+                        }
+                        else
+                        {
+                          console.warn( name + ": " + given + " ! " + when + " > " + then + ": " + thens[ then ][ 1 ]  )
+                        }
+                      }
+                      else
+                      {
+                        console.log( "not implemented" )
+                      }
+                    }
+                  )
                 }
-              } )
-            } catch ( ex ) {
-              test.error = ex
-              context.tests.push( test )
+              )
+
+              if ( thenWasCalled === false )
+              {
+                console.log( name + ": waiting..." )
+                setTimeout(
+                  function()
+                  {
+                    if ( thenWasCalled === false )
+                    {
+                      console.warn( name + ": " + given + " ! " + when + " > INCOMPLETE" )
+                    }
+                  },
+                  1000
+                )
+              }
             }
-          },
-          done: function() {
-            contexts.push( context )
-          }
-        } )
-      },
-      done: function() {
-        suiteDone( contexts )
-      }
-    } )
-  }
-}
-
-exports.Assert = {
-  equal: function( a, b ) {
-    if ( a === b ) {
-      return ""
+          )
+        }
+      )
     }
-    else {
-      return "fail"
-    }
-  }
+  )
 }
-
